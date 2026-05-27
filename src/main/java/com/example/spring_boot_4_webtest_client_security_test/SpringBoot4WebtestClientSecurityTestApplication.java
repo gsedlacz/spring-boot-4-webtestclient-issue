7b5af06ae512fec1949d9e5b411548673fa7f9f4
gsedlacz <gerrit.sedlaczek@web.de>
2026-05-27 18:00:51 +0200

    initial commit

---
+++ src/main/java/com/example/spring_boot_4_webtest_client_security_test/SpringBoot4WebtestClientSecurityTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_4_webtest_client_security_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot4WebtestClientSecurityTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot4WebtestClientSecurityTestApplication.class, args);
+	}
+
+}
